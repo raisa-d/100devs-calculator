783525bd778d3a023e7db713b81a2a0bba01afba
Raisa Dorzback <raisa.dorzback@gmail.com>
2024-01-20 13:20:31 -0500

    linked JS doc to html

--- calculator.js
+++ calculator.js
@@ -1,14 +1,8 @@
 class Calculator {
-    constructor(color, shape, hasScreen) {
-        this.color = color;
-        this.shape = shape;
-        this.hasScreen = hasScreen;
-    }
-
     add = (a, b) => a + b;
     subtract = (a, b) => a - b;
     multiply = (a, b) => a * b;
     divide = (a, b) => a / b;
 }
 
-let calc = new Calculator('black', 'square', true)
+let calc = new Calculator()
