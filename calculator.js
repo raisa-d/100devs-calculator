6b2bf6880f67e505891db4bcc543927b1efd23bd
Raisa Dorzback <raisa.dorzback@gmail.com>
2024-01-26 17:52:23 -0500

    made a new folder starting the styling from scratch. will build out the actual functionality afterwards

--- calculator.js
+++ calculator.js
@@ -6,3 +6,9 @@ class Calculator {
 }
 
 let calc = new Calculator()
+
+/*
+need to:
+1. make it so that you can click on it on document and actually use it visually
+2. allow user to make a calculation with more than 2 numbers if desired
+*/
